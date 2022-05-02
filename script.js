4f9b11d3c1e60bb030c137592a32621928520bdb
ViswaasLP <viswaas98@gmail.com>
2022-05-02 10:34:14 -0700

    Added Accordion

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-
 var video = document.getElementById("backgroundVideo");
 
+var acc = document.getElementsByClassName("penCareAccordionTrigger");
+var i;
 
 var pauseButton = document.querySelector(".pausebutton");
 
 function myFunction() {
-    if (video.paused) {
-        video.play();
-        pauseButton.innerHTML = "⏸️"
-        pauseButton.ariaLabel="Pause the video"
+  if (video.paused) {
+    video.play();
+    pauseButton.innerHTML = "⏸️";
+    pauseButton.ariaLabel = "Pause the video";
+  } else {
+    video.pause();
+    pauseButton.innerHTML = "▶️";
+    pauseButton.ariaLabel = "Play the video";
+  }
+}
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.visibility === "visible") {
+      panel.style.visibility = "hidden";
     } else {
-        video.pause();
-        pauseButton.innerHTML = "▶️"
-        pauseButton.ariaLabel="Play the video"
+      panel.style.visibility = "visible";
     }
+  });
 }
